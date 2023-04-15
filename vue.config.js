68eebeb152d08e55d1d378c4aaa2f48739a6c775
huanghaoming <986667872@qq.com>
2023-04-15 21:45:04 +0800

    2023.4.15

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  transpileDependencies: true,
+
+  // 开发模式反向代理配置，生产模式请使用 Nginx 部署并配置反向代理
+  devServer: {
+    // 你可以在此处设置开发服务器监听的端口
+    port: 8080,
+
+    proxy: {
+      '/customer': {
+        target: 'http://150.158.19.230:8849',
+        changeOrigin: true,
+      }
+    }
+  }
+}
